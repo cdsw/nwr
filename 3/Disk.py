69891fa64c8736bedcaa20684ad24dc724d4807f
cdsw <c.dswibowo@yahoo.com.tw>
2017-03-30 15:55:07 +0800

    Disk update

--- 3/Disk.py
+++ 3/Disk.py
@@ -8,40 +8,29 @@ class Disk:
         self.h = h
         self.w = w  
         self.c = c
+        self.t = t = turtle.Turtle()
     
     def showdisk(self):
-        t = turtle.Turtle()
-        t.setheading(0)
-        t.penup()
-        t.goto(self.xpos, self.ypos)
-        t.pendown()
-        t.begin_fill()
-        t.fillcolor(self.c)
-        t.goto(self.xpos + self.w / 2, self.ypos)
-        t.goto(self.xpos + self.w / 2, self.ypos + self.h)
-        t.goto(self.xpos - self.w / 2, self.ypos + self.h)
-        t.goto(self.xpos - self.w / 2, self.ypos)
-        t.goto(self.xpos, self.ypos)
-        t.penup()
-        t.end_fill()
+        self.t.setheading(0)
+        self.t.penup()
+        self.t.goto(self.xpos, self.ypos)
+        self.t.pendown()
+        self.t.begin_fill()
+        self.t.fillcolor(self.c)
+        self.t.goto(self.xpos + self.w / 2, self.ypos)
+        self.t.goto(self.xpos + self.w / 2, self.ypos + self.h)
+        self.t.goto(self.xpos - self.w / 2, self.ypos + self.h)
+        self.t.goto(self.xpos - self.w / 2, self.ypos)
+        self.t.goto(self.xpos, self.ypos)
+        self.t.penup()
+        self.t.end_fill()
 
     def newpos(self, x, y):
         self.xpos = x
         self.ypos = y
 
     def cleardisk(self):
-        t = turtle.Turtle()
-        t.setheading(0)
-        t.penup()
-        t.goto(self.xpos, self.ypos)
-        t.pendown()
-        t.pencolor("white")
-        t.begin_fill()
-        t.fillcolor("white")
-        t.goto(self.xpos + self.w / 2, self.ypos)
-        t.goto(self.xpos + self.w / 2, self.ypos + self.h)
-        t.goto(self.xpos - self.w / 2, self.ypos + self.h)
-        t.goto(self.xpos - self.w / 2, self.ypos)
-        t.goto(self.xpos, self.ypos)
-        t.penup()
-        t.end_fill()
+        self.t.setheading(0)
+        self.t.penup()
+        self.t.goto(self.xpos, self.ypos)
+        self.t.clear()
